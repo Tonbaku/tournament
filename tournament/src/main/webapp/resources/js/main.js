ddc843e3fe2c19d65dd326c773382d012ea4438b
alwin <alwin@Meruna>
2016-01-24 18:41:09 +0100

    Adjustments to layout first implementation for dropdown

--- tournament/src/main/webapp/resources/js/main.js
+++ tournament/src/main/webapp/resources/js/main.js
@@ -1,7 +1,16 @@
 function toggleCreate(){
 	var subMenu = $(".subMenu");
 	subMenu.addClass("col-xs-3")
-	subMenu.toggle();
+	subMenu.toggle("slide", {"direction" : "left"});
 	$(".previewArea").switchClass("col-xs-13", "col-xs-10");
 	
+	
+	
+}
+
+function triggerDetailAjaxUpdate(){
+	var detailForm = $("#detailForm");
+	var select = $("#selectType");
+	detailForm.append("<input name=\"toRender\" value=\"" + select.val() + "\"/>");
+	detailForm.find("input[type=submit]").click();
 }
